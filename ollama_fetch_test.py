811e536093dc255728c1456a6106c66f758c99b6
ganapathyhari <32907104+ganapathyhari@users.noreply.github.com>
2025-09-27 15:51:46 +0100

    Update ollama_fetch_test.py

--- ollama_fetch_test.py
+++ ollama_fetch_test.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["OLLAMA_API_KEY"] = "5a009cdb1a8a47daa5d8040de75ba02e.wUb5rQXusJZhdZ7LpxCAewnT"
+os.environ["OLLAMA_API_KEY"] = ""
 
 import ollama
 response = ollama.web_search("What is the capital of France?")
